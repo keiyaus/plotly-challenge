561219d5796c0259f553b5ad6b0c4cfd737d5065
Keiyau <keiyau.sin@gmail.com>
2020-09-18 19:49:52 -0400

    Updated dropdownMenu code

--- static/js/app.js
+++ static/js/app.js
@@ -1,22 +1,30 @@
-// Read in JSON file
-d3.json("././data/samples.json").then(function(data) {
+// Read in JSON file & assign variable to it
+let data = d3.json("././data/samples.json").then(function (data) {
     console.log(data);
-    
+
     // Build dropdown list content
     // Get a reference to the test subject id dropdown menu
-    var dropdownMenu = d3.select("select");
+    let dropdownMenu = d3.select("select");
 
-    // Get the list of text subject ids using the "map" method
-    var ids = data.names;
+    // Get the list of text subject ids
+    let ids = data.names;
     console.log(ids);
-    
+
     // Append each value of ids to dropdownMenu as an option
     ids.forEach(id => {
-        var idOption = dropdownMenu.append("option");
+        let idOption = dropdownMenu.append("option");
         idOption.text(id);
     });
 
-    
+    // Initialize the page with default plots & table
+    // function init() {
+    //     data.samples.filter(sample => sample === 940);
+    //     console.log(sample)
+    //     // barData = 
+    // }
+
+
 
 });
+
 
